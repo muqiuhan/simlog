75373554079e8a138b7dc3f401eb87a5e903ff90
Muqiu Han <muqiu-han@outlook.com>
2023-07-01 16:02:02 +0800

    Add builtin file printer and test it

--- test/test_simlog.ml
+++ test/test_simlog.ml
@@ -5,3 +5,19 @@ let _ =
     Log.info "Hello %s" "simlog";
     Log.error "Hey! %f" (Unix.gettimeofday ());
     Log.warn "Wuuuuu~ %d" (Thread.id (Thread.self ()))
+
+module File_Log = Simlog.Make (struct
+  include Simlog.Filter.Builtin
+  include Simlog.Formatter.Builtin
+  include Simlog.Recorder.Builtin
+
+  module Printer = Simlog.Printer.Builtin.File_Printer (struct
+    let path = "test.log"
+  end)
+end)
+
+let _ =
+    File_Log.debug "Hello";
+    File_Log.info "Hello %s" "simlog";
+    File_Log.error "Hey! %f" (Unix.gettimeofday ());
+    File_Log.warn "Wuuuuu~ %d" (Thread.id (Thread.self ()))
